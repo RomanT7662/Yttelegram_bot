a8123768d5a70a74ea67829bac9cdf648dfa8c28
RomanT7662 <139990481+RomanT7662@users.noreply.github.com>
2025-01-13 00:32:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv()
 
 import telebot
+from handlers import start_handler, filter_duration_handler, message_handler
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
-
-from handlers import start_handler, filter_duration_handler, message_handler
 
 bot.message_handler(commands=["start"])(start_handler)
 bot.message_handler(commands=["filter_duration"])(filter_duration_handler)
@@ -17,3 +15,4 @@ bot.message_handler(func=lambda message: True)(message_handler)
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
+
